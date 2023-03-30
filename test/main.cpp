09eda12f983303e2d760af8d67df91bd3578b653
Luca Venturini <luca@yepa.com>
2023-03-30 19:51:49 +0000

    Test with Boost Test in CMakeLists.txt and first test file

---
+++ test/main.cpp
@@ -0,0 +1,2 @@
+#define BOOST_TEST_MODULE Program
+#include <boost/test/unit_test.hpp>
---
+++ test/program.cpp
@@ -0,0 +1,51 @@
+#include <boost/test/unit_test.hpp>
+#include <boost/algorithm/string.hpp>
+
+#include <program.hpp>
+
+using namespace wot;
+
+BOOST_AUTO_TEST_SUITE(Program_suite)
+
+BOOST_AUTO_TEST_CASE(get_name) {
+  Program p = Program("ls");
+  std::string s = p.get_name();
+  BOOST_CHECK_EQUAL(s,"ls");
+}
+
+BOOST_AUTO_TEST_CASE(set_and_get_cli) {
+  Program p = Program("ls");
+  std::string cli = "This is a test cli";
+  p.set_cli(cli);
+  std::string s = p.get_cli();
+  BOOST_CHECK_EQUAL(s,cli);
+}
+
+BOOST_AUTO_TEST_CASE(check_and_suggest_cli) {
+  Program p = Program("ls");
+  Node n;
+  bool b = p.check_and_suggest_cli("action", n, "main_command");
+  BOOST_CHECK_EQUAL(b,true);
+}
+
+BOOST_AUTO_TEST_CASE(check_and_suggest_cli2) {
+  Program p = Program("lsssss");
+  Node n;
+  bool b = p.check_and_suggest_cli("action", n, "main_command");
+  BOOST_CHECK_EQUAL(b,false);
+}
+
+BOOST_AUTO_TEST_CASE(exec) {
+  Program p = Program("ls");
+  Node n;
+  bool b = p.check_and_suggest_cli("action", n, "main_command");
+  BOOST_CHECK_EQUAL(b,true);
+  p.set_unique("1234");
+  p.set_cli("ls -la");
+  std::string out;
+  int res = p.exec(out);
+  BOOST_CHECK_EQUAL(res,0);
+  BOOST_CHECK_EQUAL(boost::algorithm::contains(out,".."), true);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
