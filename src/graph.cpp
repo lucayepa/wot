6f3149d9bc6209187304463f379857a347b0264c
Luca Venturini <luca@yepa.com>
2023-05-25 20:05:58 +0000

    Filters that need context are applied iteratively
    Since applying a filter can remove a node, the context changes and some filters
    can give different results from new context. Now the filters are applied over
    and over until the graph is stable and nothing changes.
    
    This strategy works only if there are no context filters that pass from false to
    true due to removals of nodes.

--- src/graph.cpp
+++ src/graph.cpp
@@ -14,40 +14,47 @@ void GraphView::init() {
     LOG << "1: Node is ok. Adding to cache: " << h;
     cache[n.get_profile().get_key()].add(h);
   }
-  LOG << "Second iteration with: " << to_string(vm);
   // Second iteration: erase some nodes using filters that need context
-  std::set<std::string> hashes;
-  for(const auto & [i,hs] : cache){
-    LOG << "2: Evaluating identity key " << i;
-    auto set = hs.get();
-    MemoryHashSet new_set;
-    for(const auto & h : set) {
-      LOG << "2: Identity key has " << set.size() << " hashes to evaluate";
-      LOG << "2: Evaluating hash " << i << "->" << h;
-      NodeBase n;
-      impl.get(h,n);
-      if(check_filters(vm,n,this)) {
-        LOG << "2: Node is ok. Adding it: " << i << "->" << h;
-        new_set.insert(h);
-        hashes.insert(h);
+  // This iteration wil be done again and again until nothing is removed.
+  while(true) {
+    LOG << "Iteration with: " << to_string(vm);
+    bool something_removed = false;
+    for(const auto & [i,hs] : cache){
+      auto set = hs.get();
+      LOG << "2: Evaluating identity key " << i << " with " << set.size() <<
+        " hashes";
+      MemoryHashSet new_set;
+      for(const auto & h : set) {
+        LOG << "2: Evaluating hash " << i << "->" << h;
+        NodeBase n;
+        impl.get(h,n);
+        if(check_filters(vm,n,this)) {
+          LOG << "2: Node is ok. Adding it: " << i << "->" << h;
+          new_set.insert(h);
+        } else {
+          LOG << "2: Node is not ok. Removing from cache: " << i << "->" << h;
+          something_removed = true;
+        }
+      }
+      if(new_set.empty()) {
+        cache.erase(i);
       } else {
-        LOG << "2: Node is not ok. Removing from cache: " << i << "->" << h;
+        cache[i]=new_set;
       }
     }
-    if(new_set.empty()) {
-      cache.erase(i);
-    } else {
-      cache[i]=new_set;
-    }
+    if(!something_removed) break;
   }
   // Third iteration: add shallow identities to the remaining graph. Pay
-  // attention to use this in a badge definition graph, because shallow
+  // attention: if you use this in a badge definition graph, shallow
   // identities will receive the badge
-  if(!vm.count("all-identities")) return;
-  for(const auto & h : hashes) {
-    NodeBase n;
-    impl.get(h,n);
-    complete_cache(n);
+  if(!vm.count("all-identities") || !vm["all-identities"].as<bool>()) return;
+  for(const auto & [i,hs] : cache){
+    auto set = hs.get();
+    for(const auto & h : set) {
+      NodeBase n;
+      impl.get(h,n);
+      complete_cache(n);
+    }
   }
 }
 
